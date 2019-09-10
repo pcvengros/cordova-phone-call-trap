f149c35a3343788e0250aebaf6e8897d8ca44916
Pavol Cvengros <pavol.cvengros@securcom.me>
2019-09-10 14:14:48 +0200

    eslint

--- www/PhoneCallTrap.js
+++ www/PhoneCallTrap.js
@@ -1,34 +1,33 @@
 var exec = require('cordova/exec');
 
-exports.onCall = function(successCallback, errorCallback) {
-    if ( typeof errorCallback !== "function" ) {
+exports.onCall = function (successCallback, errorCallback) {
+    if (typeof errorCallback !== 'function') {
         errorCallback = this.errorCallback;
     }
     exec(successCallback, errorCallback, 'PhoneCallTrap', 'onCall', []);
 };
 
-exports.errorCallback = function() {
-    //console.log("WARNING: PhoneCallTrap errorCallback not implemented");
+exports.errorCallback = function () {
+    // console.log('WARNING: PhoneCallTrap errorCallback not implemented');
 };
 
-exports.getCurrentState = function(successCallback, errorCallback) {
-    if ( typeof errorCallback !== "function" ) {
+exports.getCurrentState = function (successCallback, errorCallback) {
+    if (typeof errorCallback !== 'function') {
         errorCallback = this.errorCallback;
     }
     exec(successCallback, errorCallback, 'PhoneCallTrap', 'getCurrentState', []);
 };
 
-exports.rejectCall = function(successCallback, errorCallback) {
-    if ( typeof errorCallback !== "function" ) {
+exports.rejectCall = function (successCallback, errorCallback) {
+    if (typeof errorCallback !== 'function') {
         errorCallback = this.errorCallback;
     }
     exec(successCallback, errorCallback, 'PhoneCallTrap', 'rejectCall', []);
 };
 
-exports.silenceCall = function(successCallback, errorCallback) {
-    if ( typeof errorCallback !== "function" ) {
+exports.silenceCall = function (successCallback, errorCallback) {
+    if (typeof errorCallback !== 'function') {
         errorCallback = this.errorCallback;
     }
     exec(successCallback, errorCallback, 'PhoneCallTrap', 'silenceCall', []);
 };
-
